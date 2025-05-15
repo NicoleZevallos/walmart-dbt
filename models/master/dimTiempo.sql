d56db6101526d2f6f886a19db2f7852435b00d62
NicoleZevallos <nicole_zevallos1@usmp.pe>
2025-05-15 02:06:43 -0500

    dimTiempo update

--- models/master/dimTiempo.sql
+++ models/master/dimTiempo.sql
@@ -16,3 +16,23 @@ WITH calendar AS (
 )
 
 SELECT * FROM calendar
+
+with numbers as (
+  select 0 as n
+  union all
+  select n + 1 from numbers where n + 1 < 365
+),
+fechas as (
+  select dateadd(day, n, cast('2021-01-01' as date)) as fecha
+  from numbers
+)
+select
+  fecha,
+  year(fecha) as año,
+  month(fecha) as mes,
+  day(fecha) as dia,
+  datename(weekday, fecha) as nombre_dia,
+  datename(month, fecha) as nombre_mes,
+  datepart(quarter, fecha) as trimestre
+from fechas
+option (maxrecursion 365)
