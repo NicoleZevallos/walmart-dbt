ff4cb648bcb4642c5945277f1dd454a18f2595bf
NicoleZevallos <nicole_zevallos1@usmp.pe>
2025-05-15 02:23:46 -0500

    dimTiempo update2

--- models/master/dimTiempo.sql
+++ models/master/dimTiempo.sql
@@ -1,16 +1,19 @@
-WITH fechas AS (
-  SELECT 
-    DATEADD(DAY, number, '2021-01-01') AS fecha
-  FROM master.dbo.spt_values
-  WHERE type = 'P'
-    AND number <= DATEDIFF(DAY, '2021-01-01', '2021-12-31')
+with numeros as (
+  select ROW_NUMBER() OVER (ORDER BY (select NULL)) - 1 as n
+  from (values (0),(0),(0),(0),(0),(0),(0),(0),(0),(0)) a(n)
+  cross join (values (0),(0),(0),(0),(0),(0),(0),(0),(0),(0)) b(n)
+  cross join (values (0),(0),(0),(0),(0),(0),(0),(0),(0),(0)) c(n)
 )
-SELECT
-  fecha,
-  YEAR(fecha) AS anio,
-  MONTH(fecha) AS mes,
-  DAY(fecha) AS dia,
-  DATENAME(WEEKDAY, fecha) AS nombre_dia,
-  DATENAME(MONTH, fecha) AS nombre_mes,
-  DATEPART(QUARTER, fecha) AS trimestre
-FROM fechas
+
+select
+  dateadd(day, n, cast('2021-01-01' as date)) as fecha,
+  year(dateadd(day, n, cast('2021-01-01' as date))) as año,
+  month(dateadd(day, n, cast('2021-01-01' as date))) as mes,
+  day(dateadd(day, n, cast('2021-01-01' as date))) as dia,
+  datename(weekday, dateadd(day, n, cast('2021-01-01' as date))) as nombre_dia,
+  datename(month, dateadd(day, n, cast('2021-01-01' as date))) as nombre_mes,
+  datepart(quarter, dateadd(day, n, cast('2021-01-01' as date))) as trimestre
+
+from numeros
+where n < datediff(day, '2021-01-01', '2021-12-31') + 1
+order by fecha
